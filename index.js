52bd98ef6553bc422b2af92696748138439b6eaa
Ryan Graham <ryan@strongloop.com>
2013-11-16 01:26:45 -0800

    First working version
    Accepts package name followed by a list of branches as arguments

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
-var zlib = require('zlib')
+var _    = require('lodash')
+  , http = require('http')
+  , zlib = require('zlib')
+  , url  = require('url')
   , tar  = require('tar')
   , fs   = require('fs')
 
 var gunzip = zlib.createGunzip()
 
-var pkg = 'sl-install-0.0.0.tgz' //process.args[1]
-  , dst = 'node-modules/sl-install'
+var pkg = process.argv[2]
+  , dst = 'node_modules/' + pkg
 
-var tgz    = fs.createReadStream(pkg)
-  , pkgdir = tar.Extract({path: dst})
+var pkgdir = tar.Extract({ path: dst, strip: 1 })
 
-tgz.pipe(gunzip)
-   .pipe(pkgdir)
-   .on('error', function (err) {
-      console.log('error', err)
-    })
-   .on('end', function () {
-      console.log("done")
-   })
+var repo = process.env.ALT_REPO
+
+var branches = _.rest(process.argv, 3)
+
+var urls = _.map(branches, _.curry(pkgUrl)(repo, pkg))
+
+if (!pkg || urls.length < 1) {
+  done("node index.js <pkg_name> <branch> [branch..]")
+} else {
+  console.log(urls)
+  install(urls, pkg)
+}
+
+function install(urls, name) {
+  if (urls.length < 1) {
+    return setImmediate(_.partial(done, "No more urls to try.."))
+  }
+  var nextUrl = _.first(urls)
+  http.get(url.parse(nextUrl), function (res) {
+    if (res.statusCode != '200') {
+      console.log("Failed: " + nextUrl)
+      setImmediate(_.partial(install, _.rest(urls), name))
+    } else {
+      res.pipe(gunzip)
+        .pipe(pkgdir)
+        .on('error', function (err) {
+          console.log('error ' + err)
+          setImmediate(_.partial(install, _.rest(urls), name))
+        })
+        .on('entry', function (entry) {
+          console.log(entry.path)
+        })
+        .on('end', _.partial(done, 'finished downloading from ' + nextUrl))
+    }
+  })
+}
+
+function done(msg) {
+  console.log("Done: " + msg)
+}
+
+function pkgUrl(repo, name, branch) {
+  return repo + name + '/' + branch + '/' + name + '-LATEST.tgz'
+}
